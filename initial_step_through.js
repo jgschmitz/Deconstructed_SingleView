7ce4dc2c58e24457f75c11f37f4832af125feb83
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-07-14 12:40:36 -0500

    Create initial_step_through.js

---
+++ initial_step_through.js
@@ -0,0 +1,23 @@
+// create db and insert a record
+db.foo.insert({x:123})
+
+// lets find that record
+db.foo.find({x:123})
+
+//Let show the explain plan and see there is no index
+db.foo.find({x:123}).explain()
+
+//Lets create and index on x:1
+db.foo.createIndex({x:1})
+
+//Lets show that explain plan with the index created
+db.foo.find({x:123}).explain()
+
+//We now drop the index
+db.foo.dropIndex({x:1})
+
+//Show explain plan again with index gone
+db.foo.find({x:123}).explain()
+
+//Drop record
+db.foo.remove({x:123})
