53c7ba71707e4db060f445825a340db53bd1dba6
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-08-07 13:26:02 -0500

    Create SortingName.js

---
+++ SortingName.js
@@ -0,0 +1,3 @@
+// Now lets add sorting
+// Sort by last_name, then first_name.
+db.Associates.aggregate( [{ $set: { _id: "Associates"} },{ $unionWith: { coll: "Customers", pipeline: [ { $set: { _id: "Customers"} } ] } },{ $unionWith: { coll: "Friends", pipeline: [ { $set: { _id: "Friends"} } ] } },{ $sort: { last_name: 1, first_name:1} }] )
