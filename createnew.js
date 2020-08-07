db03a9abbc663f7e4e85f5b341dd643ad5b8dc25
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-08-07 12:55:58 -0500

    Create createnew.js

---
+++ createnew.js
@@ -0,0 +1,6 @@
+// Create eWorkshop database and insert record into a collection called SingleView
+MongoDB Enterprise atlas-shard0:PRIMARY> Use eWorkshop
+Switched to db eWorkshop
+MongoDB Enterprise atlas-shard0:PRIMARY> db.practice.insert({x:123})
+WriteResult({ “nInserted” : 1 })
+MongoDB Enterprise atlas-shard0:PRIMARY>
