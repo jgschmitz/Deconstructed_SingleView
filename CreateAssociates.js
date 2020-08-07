602ef63911c9e4c82e5bc1fcf4b9ab475bfa2b7d
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-08-07 13:12:24 -0500

    Create CreateAssociates.js

---
+++ CreateAssociates.js
@@ -0,0 +1,23 @@
+use eWorkshop;
+db.Associates.insertMany(
+[{"id":1,"first_name":"Carce","last_name":"Francklin","email":"cfrancklin0@nationalgeographic.com","gender":"Male","Workplace":"Rhycero"},
+{"id":2,"first_name":"Alfredo","last_name":"Hedger","email":"ahedger1@google.com.hk","gender":"Male","Workplace":"Wordify"},
+{"id":3,"first_name":"Kile","last_name":"Moline","email":"kmoline2@cornell.edu","gender":"Male","Workplace":"Quatz"},
+{"id":4,"first_name":"Renell","last_name":"de Chastelain","email":"rdechastelain3@tinyurl.com","gender":"Female","Workplace":"Chatterpoint"},
+{"id":5,"first_name":"Corby","last_name":"Tolomei","email":"ctolomei4@1und1.de","gender":"Male","Workplace":"Topicstorm"},
+{"id":6,"first_name":"Worthy","last_name":"Beamand","email":"wbeamand5@dion.ne.jp","gender":"Male","Workplace":"Fadeo"},
+{"id":7,"first_name":"Prentiss","last_name":"Brimelow","email":"pbrimelow6@independent.co.uk","gender":"Male","Workplace":"Eabox"},
+{"id":8,"first_name":"Corabella","last_name":"Plunket","email":"cplunket7@opera.com","gender":"Female","Workplace":"Skyndu"},
+{"id":9,"first_name":"Harlene","last_name":"Goodson","email":"hgoodson8@yale.edu","gender":"Female","Workplace":"Twitterlist"},
+{"id":10,"first_name":"Libbey","last_name":"Shallcross","email":"lshallcross9@gnu.org","gender":"Female","Workplace":"Dabjam"},
+{"id":11,"first_name":"Shannah","last_name":"Packham","email":"spackhama@constantcontact.com","gender":"Female","Workplace":"Shuffletag"},
+{"id":12,"first_name":"Theresina","last_name":"McCrea","email":"tmccreab@ibm.com","gender":"Female","Workplace":"Jaloo"},
+{"id":13,"first_name":"Hesther","last_name":"Canon","email":"hcanonc@wsj.com","gender":"Female","Workplace":"Yotz"},
+{"id":14,"first_name":"Madel","last_name":"Grenshiels","email":"mgrenshielsd@hugedomains.com","gender":"Female","Workplace":"Mudo"},
+{"id":15,"first_name":"Susanne","last_name":"Hulburd","email":"shulburde@dot.gov","gender":"Female","Workplace":"Gabtune"},
+{"id":16,"first_name":"Marty","last_name":"Batie","email":"mbatief@sohu.com","gender":"Male","Workplace":"Ntag"},
+{"id":17,"first_name":"King","last_name":"Swindells","email":"kswindellsg@bizjournals.com","gender":"Male","Workplace":"Plambee"},
+{"id":18,"first_name":"Mathian","last_name":"Ruzicka","email":"mruzickah@smh.com.au","gender":"Male","Workplace":"Gigashots"},
+{"id":19,"first_name":"Judye","last_name":"Flescher","email":"jflescheri@tumblr.com","gender":"Female","Workplace":"Youfeed"},
+{"id":20,"first_name":"Kip","last_name":"Orwin","email":"korwinj@geocities.jp","gender":"Female","Workplace":"Twimbo"}]
+);
