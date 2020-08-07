4072806f6938d3f356fcd1348c0245ea9a31f6e8
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-08-07 13:24:00 -0500

    Create CreateAggregate.js

---
+++ CreateAggregate.js
@@ -0,0 +1,10 @@
+// Now, to begin creating this aggregation of people you know, let’s combine
+// all three collections of data.
+// First, we’ll use a $set stage to create a named group of data for your Associates
+db.Associates.aggregate( [{ $set: { _id: "Associates"} }]);
+
+// Now, we will create the results of the union between Associates and Customers.
+db.Associates.aggregate( [{ $set: { _id: "Associates"} },{ $unionWith: { coll: "Customers", pipeline: [ { $set: { _id: "Customers"} } ] } }] );
+
+// Now, we will create the results of the union between Associates, Customers, and Friends.
+db.Associates.aggregate( [{ $set: { _id: "Associates"} },{ $unionWith: { coll: "Customers", pipeline: [ { $set: { _id: "Customers"} } ] } },{ $unionWith: { coll: "Friends", pipeline: [ { $set: { _id: "Friends"} } ] } }] )
