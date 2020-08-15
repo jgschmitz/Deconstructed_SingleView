30a9f55c16b328d2ac2c6cf0f89658899c98321c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-08-15 08:07:02 -0500

    Update CreateAggregate.js
    Cleaned up some white space

--- CreateAggregate.js
+++ CreateAggregate.js
@@ -1,4 +1,4 @@
-// Now, to begin creating this aggregation of people you know, let’s combine
+// Now, to begin creating this aggregation of people you know, Now combine
 // all three collections of data.
 // First, we’ll use a $set stage to create a named group of data for your Associates
 db.Associates.aggregate( [{ $set: { _id: "Associates"} }]);
