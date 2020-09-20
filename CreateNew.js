ae113f90781d4e4e71f219982ccbf8efd08c2cf5
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-09-20 09:21:46 -0500

    Update CreateNew.js
    updated insert record to marco polo -

--- CreateNew.js
+++ CreateNew.js
@@ -1,3 +1,3 @@
 // Create eWorkshop database and insert record into a collection called SingleView
 Use eWorkshop;
-db.practice.insert({x:123});
+db.practice.insert({"id":1,"first_name":"Marco","last_name":"Polo","email":"mpolo0@github.com","gender":"Male","ip_address":"197.164.91.169"});
